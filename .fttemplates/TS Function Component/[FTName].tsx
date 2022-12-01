f51823deb0c4895ba74421784fcf4820279e6c9a
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2022-12-01 15:06:57 +0700

    +add: create folder template

---
+++ .fttemplates/TS Function Component/[FTName].tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface <FTName | pascalcase>Props {}
+
+const <FTName | pascalcase>: React.FC<<FTName | pascalcase>Props> = (props) => {
+  return <div className="<FTName | pascalcase>-root"><FTName | pascalcase> component</div>;
+};
+
+export default <FTName | pascalcase>;
